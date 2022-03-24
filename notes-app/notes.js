e03d89714e5d82f08237af940c45268bfd0de182
Anıl Aydın <anil_aydinn@outlook.com>
2022-03-24 18:06:00 +0300

    Add list notes.

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -32,6 +32,15 @@ const removeNote = (title) => {
   }
 };
 
+const listNotes = () => {
+  const notes = loadNotes();
+  console.log(chalk.inverse("Your notes"));
+  notes.forEach((note) => {
+    console.log("Title: ", note.title, "\n", "Body: ", note.body);
+    console.log("---------------------------------------------------------");
+  });
+};
+
 const saveNotes = (notes) => {
   const dataJSON = JSON.stringify(notes);
   fs.writeFileSync("notes.json", dataJSON);
@@ -51,4 +60,5 @@ module.exports = {
   getNotes: getNotes,
   addNote: addNote,
   removeNote: removeNote,
+  listNotes: listNotes,
 };
